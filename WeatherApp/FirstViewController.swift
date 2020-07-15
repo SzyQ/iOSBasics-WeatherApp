2991d5ec3553162a68209166fde9dafd6822a336
Szymon Klimaszewski <szymon@unum.la>
2020-07-15 21:52:50 +0200

    Initial Commit

---
+++ WeatherApp/FirstViewController.swift
@@ -0,0 +1,20 @@
+//
+//  FirstViewController.swift
+//  WeatherApp
+//
+//  Created by Work on 15/07/2020.
+//  Copyright © 2020 Szymon Klimaszewski. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ WeatherApp/SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  WeatherApp
+//
+//  Created by Work on 15/07/2020.
+//  Copyright © 2020 Szymon Klimaszewski. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
