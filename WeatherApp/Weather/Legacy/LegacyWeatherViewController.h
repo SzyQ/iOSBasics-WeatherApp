532123d4316e0343b4e6fee8bcf90cd94b07e14e
Szymon Klimaszewski <szymon@unum.la>
2020-07-24 22:05:03 +0200

    Display map

---
+++ WeatherApp/Weather/Legacy/LegacyWeatherViewController.h
@@ -0,0 +1,22 @@
+//
+//  LegacyWeatherViewController.h
+//  WeatherApp
+//
+//  Created by Work on 24/07/2020.
+//  Copyright © 2020 Szymon Klimaszewski. All rights reserved.
+//
+
+#ifndef LegacyWeatherViewController_h
+#define LegacyWeatherViewController_h
+
+
+#endif /* LegacyWeatherViewController_h */
+
+#import <UIKit/UIKit.h>
+#import <GoogleMaps/GoogleMaps.h>
+
+@interface LegacyWeatherViewController : UIViewController
+
+@property (weak, nonatomic) IBOutlet GMSMapView *mapView;
+
+@end
